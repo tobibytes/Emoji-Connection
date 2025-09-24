f73be18a47b0f27db2a92161adcfb3a87071b159
Oluwatobi <oldwhisper98@icloud.com>
2025-09-24 00:17:06 -0400

    initalized connection manager

--- client.py
+++ client.py
@@ -1,14 +1,18 @@
 import socket
-PORT = 60006
+PORT = 60007
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(("127.0.0.1", PORT))
 
-while True:
-    data = client.recv(1024).decode('utf-8')
-    if data[-2:] == "-1":
-        print("skip received: ", data)
-        continue
-    print("received: ", data)
-    send_data = input("Send: ")
-    client.send(send_data.encode('utf-8'))
+try:
+    
+    while True:
+        data = client.recv(1024).decode('utf-8')
+        if data[-2:] == "-1":
+            print("skip received: ", data[:-2])
+            continue
+        print("received: ", data)
+        send_data = input("Send: ")
+        client.send(send_data.encode('utf-8'))
+except KeyboardInterrupt:
+    client.close()
--- server.py
+++ server.py
@@ -1,37 +1,54 @@
 import threading
 import socket
 from utils import is_valid_username
+from typing import List, Dict
 host = "127.0.0.1"
-PORT = 60006
+PORT = 60007
 
 
 class ConnectionManager:
     def __init__(self):
-        self.user_to_connection = {}
-        self.connnection_to_user = {}
+        self.user_to_connection: Dict[str, socket.socket] = {}
+        self.connnection_to_user: Dict[socket.socket, str] = {}
     
     def print_connections_db(self):
         print('connections: ', self.user_to_connection)
     
+    def send_to_user(self, username: str, message: str):
+        if username not in self.user_to_connection:
+            print("could not find user")
+        conn = self.user_to_connection[username]
+        conn.send(message.encode('utf-8'))
+        
+    def get_users(self) -> List[str]:
+        return list(self.user_to_connection.keys())
+
     def get_username(self, conn: socket.socket):
         conn.send("Type in your username: ".encode('utf-8'))
         username = conn.recv(1024).decode('utf-8')
         if is_valid_username(username):
             self.user_to_connection[username] = conn
             self.connnection_to_user[conn] = username
+        conn.send(f"Enjoy the game {username} -1".encode('utf-8'))
 
+    def terminate(self, conn: socket.socket):
+        user = self.connnection_to_user[conn]
+        del self.connnection_to_user[conn]
+        del self.user_to_connection[user]
+        conn.close()
 
 def handle_connection(conn: socket.socket, addr, conn_manager: ConnectionManager):
     conn.send("Hello from server -1".encode('utf-8'))
     conn_manager.get_username(conn)
-    conn_manager.print_connections_db()
-    
-    while True:
-        recv_data = conn.recv(1024).decode('utf-8')
-        print(f"recieved from {conn_manager.connnection_to_user[conn]}: {recv_data}")
-        conn.send("processed it".encode('utf-8'))
-    
-    
+    start_message = "Welcome to Emoji Con\nFind the path from 😎 -----> 🤔"
+    conn_manager.send_to_user(conn_manager.connnection_to_user[conn], start_message)
+    try:
+        while True:
+            recv_data = conn.recv(1024).decode('utf-8')
+            print(f"recieved from {conn_manager.connnection_to_user[conn]}: {recv_data}")
+            conn.send("processed it".encode('utf-8'))
+    except:
+        conn_manager.terminate(conn)
     
 def main():
     connection_manager = ConnectionManager()
@@ -39,10 +56,14 @@ def main():
     server.bind((host, PORT))
     print("started listening")
     server.listen()
-    while True:
-        conn, addr = server.accept()
-        t = threading.Thread(target=handle_connection, args=(conn, addr, connection_manager))
-        t.start()
+    try:
+        while True:
+            conn, addr = server.accept()
+            t = threading.Thread(target=handle_connection, args=(conn, addr, connection_manager))
+            t.start()
+    except:
+        server.close()
+        
 
 
 if __name__ == "__main__":
